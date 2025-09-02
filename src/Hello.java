96a3651097921a08b121d562ecd32025e910c83c
Robert <robertpschmidt42@gmail.com>
2025-09-02 17:22:25 -0500

    Add my name (Robert) and major to the code. Also changed the spacing.

--- src/Hello.java
+++ src/Hello.java
@@ -8,11 +8,15 @@
 
 
 public class Hello {
-
 	
-		  public static void main(String args[]) {
-		    System.out.println("Ty Dresden\n");
-		    System.out.println("I am a Computer Science major\n");
-		  }
+	public static void main(String args[]) {
+		
+		    System.out.println("Ty Dresden");
+		    System.out.println("I am a Computer Science major");
+		    
+		    System.out.println("And I am Robert Schmidt");
+		    System.out.println("My major is Computer Engineering!");
 
-		}
+	}
+
+}
